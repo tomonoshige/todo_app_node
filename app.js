67e29c9f1b40b89f370df3645cc1d4293bbb3ab9
tomonoshige <sophisticated0923@gmail.com>
2021-06-13 14:06:13 +0900

    bcryptパッケージのインストール（パスワードのハッシュ化）

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const session = require('express-session');
+const bcrypt = require('bcrypt');
 
 const app = express();
 app.use(express.static('public'));
